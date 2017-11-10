242b6b7cc820bd34bd4fe29517a115618842ba40
KASSIANO RESENDE <sn1041520@cfp127aluno.local>
2017-11-10 16:30:47 -0200

    first commit

---
+++ api.php
@@ -0,0 +1,75 @@
+<?php
+
+//conexão
+$conn = mysqli_connect('localhost', 'root', 'bcd127', 'dbTesteAPI');
+
+
+// pegar o metodo da chamada, POST ou GET
+$method = $_SERVER['REQUEST_METHOD'];
+
+//Via POST faremos o update e o insert
+if ($method == "POST"){
+
+  $acao = $_POST["acao"];
+
+  $tabela = $_POST["tabela"];
+  $valores = $_POST["valores"];
+
+  $sql = "";
+  if ($acao == "INSERT") {
+      $sql = "INSERT INTO $tabela set $valores;";
+  }
+
+  if ($acao == "UPDATE") {
+     $sql = "UPDATE $tabela set $valores;";
+  }
+
+  //Executar ação no banco
+  $result = mysqli_query($conn, $sql);
+
+  echo $result;
+
+}else if ($method == "GET"){
+  //via get faremos o select e o delete
+
+    $acao = $_GET["acao"];
+
+    $tabela = $_GET["tabela"];
+    $where = $_GET["where"];
+
+    $sql = "";
+    if ($acao == "SELECT") {
+
+        $sql = "SELECT * from $tabela $where;";
+
+        //Executar ação no banco
+        $result = mysqli_query($conn, $sql);
+
+        $arrayRetorno = array();
+        if (mysqli_num_rows($result) > 0) {
+
+            while($item = mysqli_fetch_assoc($result)) {
+                $arrayRetorno[] = $item;
+            }
+
+        } else {
+            echo json_encode($arrayRetorno);
+        }
+    }
+
+    if ($acao == "DELETE") {
+       $sql = "DELETE from $tabela $where;";
+
+       //Executar ação no banco
+       $result = mysqli_query($conn, $sql);
+
+       echo $result ;
+    }
+
+}else{
+
+  echo json_encode(array("erro" => true , "mensagem" => "metodo não suportado"));
+
+}
+
+?>
